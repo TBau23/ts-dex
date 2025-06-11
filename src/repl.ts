be80beed79cf3adfdb96368acaa53c82b6e57f04
TBau23 <tom.a.bauer@protonmail.com>
2025-06-11 17:49:01 -0500

    i/o

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,25 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
-    const x: string[] = input.trim().split(' ').filter(x => x !== '');
+    const x: string[] = input.trim().split(' ').filter(x => x !== '').map(x => x.toLowerCase());
     return x;
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Pokedex > '
+    });
+
+    rl.prompt();
+    rl.on("line", (input) => {
+        const clean = cleanInput(input);
+        if (clean.length == 0 ) {
+            rl.prompt();
+        }
+        console.log(`Your command was: ${clean[0]}`);
+        rl.prompt();
+    });
+
 }
