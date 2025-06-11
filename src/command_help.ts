7aa235622dc805656588900ed420202d9866e2bc
TBau23 <tom.a.bauer@protonmail.com>
2025-06-11 18:19:31 -0500

    commands

---
+++ src/command_help.ts
@@ -0,0 +1,9 @@
+export function commandHelp() {
+    console.log(`
+        Welcome to the Pokedex!
+        Usage:
+
+        help: Displays a help message
+        exit: Exits the Pokedex
+        `)
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,29 @@
 import { createInterface } from "readline";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+
+export type CLICommand = {
+    name: string;
+    description: string;
+    callback: (commands: Record<string, CLICommand>) => void;
+}
+
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        exit: {
+            name: "exit",
+            description: "Exit the Pokedex",
+            callback: commandExit,
+        },
+        help: {
+            name: "help",
+            description: "Display a help message",
+            callback: commandHelp,
+        }
+    }
+}
+
+
 
 export function cleanInput(input: string): string[] {
     const x: string[] = input.trim().split(' ').filter(x => x !== '').map(x => x.toLowerCase());
@@ -18,7 +43,13 @@ export function startREPL() {
         if (clean.length == 0 ) {
             rl.prompt();
         }
-        console.log(`Your command was: ${clean[0]}`);
+        // should probably check something other than just the first word
+        const command = getCommands()[clean[0]];
+        if (command) {
+            command.callback(getCommands());
+        } else {
+            console.log(`Unknown command: ${clean[0]}`);
+        }
         rl.prompt();
     });
 
